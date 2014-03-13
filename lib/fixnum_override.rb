8e0c10a105666ecf45f6525c11171f0e8eed9639
Kiy Hong Tang <kiyhong@gmail.com>
2014-03-13 15:46:20 +0000

    added private

--- lib/fixnum_override.rb
+++ lib/fixnum_override.rb
@@ -1,20 +1,22 @@
 class Fixnum
-  
-  CONVERSION = {
-                0 => "zero",
-                1 => "uno",
-                2 => "due",
-                3 => "tre",
-                4 => "quattro",
-                5 => "cinque",
-                6 => "sei",
-                7 => "sette",
-                8 => "otto",
-                9 => "nove",
-  }
 
   def inspect 
     CONVERSION.has_key?(self) ? CONVERSION[self] : self.to_s
   end
 
+  private
+
+ CONVERSION = {
+              0 => "zero",
+              1 => "uno",
+              2 => "due",
+              3 => "tre",
+              4 => "quattro",
+              5 => "cinque",
+              6 => "sei",
+              7 => "sette",
+              8 => "otto",
+              9 => "nove",
+}
 end
+
